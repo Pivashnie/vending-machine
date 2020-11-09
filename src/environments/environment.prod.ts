22ca9febbc06a5d871122dc9cbed0cf74cc743b8
pivashnie <pivashnie.pillay@gmail.com>
2020-11-09 17:50:23 +0200

    preparing for deployment to github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDZJzfAq1PfmRrmm1-JyAcTHzA0Ef8n4f0",
+    authDomain: "vending-machine-c0403.firebaseapp.com",
+    databaseURL: "https://vending-machine-c0403.firebaseio.com",
+    projectId: "vending-machine-c0403",
+    storageBucket: "vending-machine-c0403.appspot.com",
+    messagingSenderId: "852505446668",
+    appId: "1:852505446668:web:c590474ce4213e0ec76129",
+    measurementId: "G-2Q4M1BYFRG"
+  }
 };
